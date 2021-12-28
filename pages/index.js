7888bbe8b268e21ad84ee35d52bd3f5909b95c25
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2021-12-28 11:57:39 +0530

    added favicon

--- pages/index.js
+++ pages/index.js
@@ -129,6 +129,8 @@ export default function Home() {
       <Head>
         <title>Vidyartha</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png"/>
         <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCLAaadQJ2iA8m6Nq2KGAQXwL9B6CwVvZ8&libraries=places"></script>
       </Head>
       <main className="main-banner">
