791ad1672ba8db6778c90f6744be79c628c23581
Vishal Singh <vishalsingh.mail0503@gmail.com>
2021-12-28 14:24:24 +0530

    responsive footer, accordion and steps

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,15 @@ import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Timeline from '@material-ui/lab/Timeline';
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
+import TimelineDot from '@material-ui/lab/TimelineDot';
 
 export default function Home() {
   return (
@@ -26,15 +35,53 @@ export default function Home() {
       </div>
     </div>
 
-    <div className="item">
+    {/* <div className="item">
 
-    </div>
+    </div> */}
 
     <div className="step-wrap">
       <p>Donate in three simple steps</p>
+      <Grid container spacing={4}>
+        <Grid item xs={12} sm={6} className="time-wrap">
+          <Timeline>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent>
+                <Typography className="time-head">Search for your school</Typography>
+                <Typography className="time-body">Search for your school by entering the location and name of the school.</Typography>
+              </TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+                <TimelineConnector />
+              </TimelineSeparator>
+              <TimelineContent>
+                <Typography className="time-head">Share with others</Typography>
+                <Typography className="time-body">Help us in spreading this campaign and encourage others to donate.</Typography>                
+              </TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot />
+              </TimelineSeparator>
+              <TimelineContent>
+                <Typography className="time-head">Click on donate</Typography>
+                <Typography className="time-body">Your donations will safely reach to us and once completed, we will donate books to the school.</Typography>                
+              </TimelineContent>
+            </TimelineItem>
+          </Timeline>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          
+        </Grid>
+      </Grid>
     </div>
 
-    <div className="video-wrap">
+    {/* <div className="video-wrap">
       <p>Watch this short video on importance of spiritual books.</p>
     </div>
 
@@ -43,155 +90,155 @@ export default function Home() {
     </div>
 
     <div className="tm-wrap">
-      <p>Testimonials</p>
-    </div>
+       <p>Testimonials</p>
+    </div> */}
 
     <div className="ques-wrap">
       <p>FAQ</p>
       <Accordion>
-        <AccordionSummary
+        <AccordionSummary className="acc-sum"
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
           <Typography className="accor">1. What is the purpose of Vidyartha?</Typography>
         </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
+        <AccordionDetails className="acc-det">
+          <Typography className="acc-par">
             The purpose of the Vidyartha is to make spiritual wisdom literature available in the school libraries. Children need a stronng foundation of moral values, the ability to handle emotionally turbulent situations, strong determination, and healthy habits, all these needs can be effectively fulfilled by spiritual literature. They also instill within us healthy pride about our own native culture and heritage and explain the deeper meanings behind them. Vidyartha is committed to gift this literature to the schools.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion>
-        <AccordionSummary
+        <AccordionSummary className="acc-sum"
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2a-content"
           id="panel2a-header"
         >
           <Typography className="accor">2. How does Vidyartha work?</Typography>
         </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
+        <AccordionDetails className="acc-det">
+          <Typography className="acc-par">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
             sit amet blandit leo lobortis eget.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion>
-        <AccordionSummary
+        <AccordionSummary className="acc-sum"
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2a-content"
           id="panel2a-header"
         >
           <Typography className="accor">3. What books will be gifted to the school?</Typography>
         </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
+        <AccordionDetails className="acc-det">
+          <Typography className="acc-par">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
             sit amet blandit leo lobortis eget.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion>
-        <AccordionSummary
+        <AccordionSummary className="acc-sum"
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2a-content"
           id="panel2a-header"
         >
           <Typography className="accor">4. What if you can not complete your targeted amount?</Typography>
         </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
+        <AccordionDetails className="acc-det">
+          <Typography className="acc-par">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
             sit amet blandit leo lobortis eget.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion>
-        <AccordionSummary
+        <AccordionSummary className="acc-sum"
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2a-content"
           id="panel2a-header"
         >
           <Typography className="accor">5. What if the school refuses to accept the books?</Typography>
         </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
+        <AccordionDetails className="acc-det">
+          <Typography className="acc-par">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
             sit amet blandit leo lobortis eget.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion>
-        <AccordionSummary
+        <AccordionSummary className="acc-sum"
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2a-content"
           id="panel2a-header"
         >
           <Typography className="accor">6. What is the purpose of Vidyartha?</Typography>
         </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
+        <AccordionDetails className="acc-det">
+          <Typography className="acc-par">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
             sit amet blandit leo lobortis eget.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion>
-        <AccordionSummary
+        <AccordionSummary className="acc-sum"
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2a-content"
           id="panel2a-header"
         >
           <Typography className="accor">7. How does Vidyartha work?</Typography>
         </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
+        <AccordionDetails className="acc-det">
+          <Typography className="acc-par">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
             sit amet blandit leo lobortis eget.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion>
-        <AccordionSummary
+        <AccordionSummary className="acc-sum"
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2a-content"
           id="panel2a-header"
         >
           <Typography className="accor">8. What books will be gifted to the school?</Typography>
         </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
+        <AccordionDetails className="acc-det">
+          <Typography className="acc-par">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
             sit amet blandit leo lobortis eget.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion>
-        <AccordionSummary
+        <AccordionSummary className="acc-sum"
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2a-content"
           id="panel2a-header"
         >
           <Typography className="accor">9. What if you can not complete your targeted amount?</Typography>
         </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
+        <AccordionDetails className="acc-det">
+          <Typography className="acc-par">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
             sit amet blandit leo lobortis eget.
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion>
-        <AccordionSummary
+        <AccordionSummary className="acc-sum"
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2a-content"
           id="panel2a-header"
         >
           <Typography className="accor">10. What if the school refuses to accept the books?</Typography>
         </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
+        <AccordionDetails className="acc-det">
+          <Typography className="acc-par">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
             sit amet blandit leo lobortis eget.
           </Typography>
@@ -213,6 +260,19 @@ export default function Home() {
         </Button>
       </div>
     </div>
+    <footer className="foot-wrap">
+      <Grid container spacing={4}>
+        <Grid item xs={12} sm={4}>
+          <p>Terms & Conditions</p>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <p>Privacy Policy</p>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <p>Return Policy</p>
+        </Grid>
+      </Grid>
+    </footer>
   </div>
   )
 }
