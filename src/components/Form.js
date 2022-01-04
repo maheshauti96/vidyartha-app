5b8de8da504140bd091a345036d9fe3f639a24c8
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2022-01-04 15:50:15 +0530

    contact us form modified

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,54 +1,52 @@
 import { Accordion, AccordionSummary, Button, TextField, Typography, AccordionDetails } from "@material-ui/core";
 import { useState, useRef, useEffect } from 'react';
+import { postFeedback, isValidEmail } from "../services/service";
 
 export default function Form() {
 
-    const [Name, setName] = useState("");
-    const [Email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
-    const [Feedback, setFeedback] = useState("");
+    const [feedback, setFeedback] = useState("");
 
     let handleSubmit = async (e) => {
         e.preventDefault();
-        try {
-            let res = await fetch("https://api.vidyartha.org/shastradaan/contactus/contactus", {
-                method: "POST",
-                body: JSON.stringify({
-                    Name: Name,
-                    Email: Email,
-                    Feedback: Feedback,
-                }),
-            });
-            let resJson = await res.json();
-            if (res.status === 200) {
-                setName("");
-                setEmail("");
-                setFeedback("Feedback received");
-            } else {
-                setMessage("Some error occured");
-            }
-        } catch (err) {
-            console.log(err);
+
+        if(!isValidEmail(email)){
+            setMessage("Enter a valid email");
+            return;
+        }
+
+        let res = await postFeedback(name, email, feedback);
+        
+        if(res.status == 200) {
+            setMessage("Thank you for your feedback");
+            setName("");
+            setEmail("");
+            setFeedback("");
+        }
+        else {
+            setMessage("Some error occured");
         }
     };
     return (
         <div className="cont-wrap">
             <h4>Contact Us</h4>
             <div className="form1-wrap">
-                <TextField className="id1" label="Name" value={Name} variant="outlined" onChange={(e) => setName(e.target.value)}
-                />
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <TextField className="id1" label="Name" value={name} variant="outlined" onChange={(e) => {setName(e.target.value), setMessage("")}}
+                required />
                 <br />
-                <TextField className="id1" label="Email" value={Email} variant="outlined" onChange={(e) => setEmail(e.target.value)}
-                />
+                <TextField className="id1" label="Email" value={email} variant="outlined" onChange={(e) => {setEmail(e.target.value), setMessage("")}}
+                required />
                 <br />
-                <textarea className="id2" type="text" value={Feedback} placeholder="Feedback" cols="auto" rows="10" onChange={(e) => setFeedback(e.target.value)}
-                />
+                <textarea className="id2" type="text" value={feedback} placeholder="Feedback" cols="auto" rows="10" onChange={(e) => {setFeedback(e.target.value), setMessage("")}}
+                required />
                 <br />
-                <Button className="btn1" variant="contained">
+                <Button className="btn1" variant="contained" type="submit">
                     <p>Submit</p>
-
-
                 </Button>
+            </form>
                 <div className="message">{message ? <p>{message}</p> : null}</div>
 
             </div>
