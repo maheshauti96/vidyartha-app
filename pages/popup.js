d81ae44dfdc3758de095ff9a7dcfc516021f0b0a
Vishal Singh <vishalsingh.mail0503@gmail.com>
2022-01-20 21:06:30 +0530

    carousel corrected

--- pages/popup.js
+++ pages/popup.js
@@ -4,9 +4,9 @@ import { ArrowForwardIos } from '@material-ui/icons';
 export default function Popup() {
     return (<div className="main">
         
-        {/* <h1 className="heading">HELP IN CREATING A</h1>
+        {/* <p className="heading">HELP IN CREATING A</p>
 
-        <h1 className="heading1">BRIGHTER FUTURE</h1>
+        <p className="heading1">BRIGHTER FUTURE</p>
         <h2 className="heading2">BY DONATING SPRITUAL BOOKS TO THE CHILDREN</h2>
         <hr className="line"></hr>
         <h3 className="data">Reading spritual books helps in developing personality,
@@ -27,15 +27,17 @@ export default function Popup() {
         >Donate Now</Button></div> */}
 
         <div className="center-align">
-            <h1>HELP IN CREATING A BRIGHTER FUTURE</h1>
-            <h3>BY DONATING SPIRITUAL BOOKS TO THE CHILDREN</h3>
+            <p className="head">HELP IN CREATING A</p>
+            <p className="head">BRIGHTER FUTURE</p>
+            <p className="sub-head">BY DONATING SPIRITUAL BOOKS TO THE CHILDREN</p>
             <hr />
-            <p>Reading spiritual books helps in developing personality, builds self esteem, increases moral values, concentration, knowledge and peace</p>
+            <p className="par">Reading spiritual books helps in developing personality, builds self esteem, increases moral values, concentration, knowledge and peace</p>
             <Button className="btn1" variant="contained">
                 <p>Submit</p>
             </Button>
+            <div className="image"><img className="center-align image" src="/book and bg-01.png"></img></div>
         </div>
-        <div><img className="image" src="image1.png"></img></div>
+        {/* <div className="image"><img className="center-align image" src="/book and bg-01.png"></img></div> */}
         <div className="logo"><img src="logo-white.png"></img></div>
         
     </div>)
