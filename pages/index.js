b829776c14bf8fe3a915d298482f7aaacecdf51e
Mahesh Auti <maheshauti00@gmail.com>
2022-02-12 15:09:30 +0530

    resolved conflicts

--- pages/index.js
+++ pages/index.js
@@ -423,17 +423,16 @@ export default function Home() {
         <Grid item xs={12} sm={3}>
            <p><Link href="/privacypolicy"><span style={{cursor:"pointer"}}>Privacy Policy</span></Link></p>
         </Grid>
-<<<<<<< HEAD
-        <Grid item xs={12} sm={2}>
-          <p>About Us</p>
-        </Grid>
-        <Grid item xs={12} sm={2}>
-          <p>Contact Us</p>
-=======
         <Grid item xs={12} sm={3}>
           <p><Link href="/returnpolicy"><span style={{cursor:"pointer"}}>Return Policy</span></Link></p>
->>>>>>> 2c448423ce47d452a91030913b79137b1538f49f
         </Grid>
+        <Grid item xs={12} sm={2}>
+        <p><Link href="/aboutus"><span style={{cursor:"pointer"}}>About Us</span></Link></p>
+        </Grid>
+        <Grid item xs={12} sm={2}>
+        <p><Link href="/contactus"><span style={{cursor:"pointer"}}>Contact Us</span></Link></p>
+        </Grid>
+
       </Grid>
     </footer>
     </div>
