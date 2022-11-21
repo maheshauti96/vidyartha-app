1193764280f63275b876dbc1172c042aa7d71cdf
amitzensar27 <amit.singh1@zensar.com>
2022-11-21 17:14:33 +0530

    Remove unused code

--- src/services/service.js
+++ src/services/service.js
@@ -118,15 +118,4 @@ export const isValidEmail = (e = '') => {
   return true;
 };
 
-export function generateRandomString(stringLength = 10) {
-  let randomString = '';
-  let randomAscii;
-  const asciiLow = 65;
-  const asciiHigh = 90
-  for (let i = 0; i < stringLength; i += 1) {
-    randomAscii = Math.floor((Math.random() * (asciiHigh - asciiLow)) + asciiLow);
-    randomString += String.fromCharCode(randomAscii)
-  }
-  return randomString
-}
 
