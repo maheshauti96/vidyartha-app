c891173f54c80cb1ce4947784ed3715ac52050b3
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2023-09-01 19:02:39 +0530

    feat : added FAQ questions

--- src/components/new/FAQ.js
+++ src/components/new/FAQ.js
@@ -2,48 +2,38 @@ import React from "react";
 import { Accordion, AccordionSummary, AccordionDetails, Typography } from "@material-ui/core";
 import { ExpandMore } from "@material-ui/icons";
 const FAQ = () => {
-    const FAQs = [
-        {
-          ques: "Lorem ipsum dolor sit amet?",
-          ans: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi."
-        },
-        {
-          ques: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas?",
-          ans: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo."
-        },
-        {
-          ques: "Maecenas accumsan eros nec libero commodo tincidunt?",
-          ans: "Maecenas accumsan eros nec libero commodo tincidunt. Nullam vel sem. Praesent sodales velit quis augue. Cras suscipit, urna at aliquam rhoncus, urna quam viverra nisi, in interdum massa nibh nec erat."
-        },
-        {
-          ques: "Fusce vulputate eleifend sapien?",
-          ans: "Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Aenean vestibulum velit id placerat dapibus."
-        },
-        {
-          ques: "Cras nec ante?",
-          ans: "Cras nec ante. Pellentesque a nulla. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam tincidunt urna. Nulla ullamcorper vestibulum turpis."
-        },
-        {
-          ques: "Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem?",
-          ans: "Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo."
-        },
-        {
-          ques: "In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo?",
-          ans: "In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi."
-        },
-        {
-          ques: "Nullam dictum felis eu pede mollis pretium?",
-          ans: "Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim."
-        },
-        {
-          ques: "Vivamus elementum semper nisi?",
-          ans: "Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus."
-        },
-        {
-          ques: "Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus?",
-          ans: "Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue."
-        }
-      ];
+  const FAQs = [
+    {
+      question: "What is the purpose of Vidyartha?",
+      answer:
+        "The purpose of the Vidyartha is to make spiritual wisdom literature available in the school libraries. Children need a strong foundation of moral values, the ability to handle emotionally turbulent situations, strong determination, and healthy habits, all these needs can be effectively fulfilled by spiritual literature. They also instill within us healthy pride about our own native culture and heritage and explain the deeper meanings behind them. Vidyartha is committed to gift this literature to the schools.",
+    },
+    {
+      question: "How does Vidyartha work?",
+      answer:
+        "The Vidyartha is a Crowdfunding platform where alumni can find their school & sponsor their choice amount to gift spiritual literature as gratitude for their schools. They can also share this campaign with other alumni to quickly complete the target.",
+    },
+    {
+      question: "What books will be gifted to the school?",
+      answer:
+        "Spiritual books like Ramayana, Mahabharata, Srimad Bhagavatam, Bhagavad Gita, many other value education, story and picture books will be gifted to the schools.",
+    },
+    {
+      question: "What if you can not complete your targeted amount?",
+      answer:
+        "After the set target date whatever amount is collected worth those amount of books will be gifted to the school.",
+    },
+    {
+      question: "What if the school refuses to accept the books?",
+      answer:
+        "If you are doubtful about acceptance of the books by your school, you may once inform the school about your desire to gift them books and check their response. From our side too, if some school has any concerns we shall try our best to address those concerns, if still, any particular school is not willing to take the books then they will be given to some other interested school.",
+    },
+    {
+      question: "What if I am not able to find my school?",
+      answer:
+        "Please search the name of your school on Google Maps and try the same name.",
+    },
+  ]
       
 
     return (
@@ -51,19 +41,19 @@ const FAQ = () => {
             <h3 className="section-title" >FAQ</h3>
             <div className="faq-container" >
             {
-                FAQs.map(({ques , ans}) => (
-                    <Accordion className="">
+                FAQs.map(({question , answer}) => (
+                    <Accordion key={question} className="">
                         <AccordionSummary
                             expandIcon={<ExpandMore />}
                             aria-controls="panel1a-content"
                             id="panel1a-header"
                             className="accordian-summary"
                         >
-                            <Typography className="question">{ques}</Typography>
+                            <Typography className="question">{question}</Typography>
                         </AccordionSummary>
                         <AccordionDetails className="accordian-details">
                             <Typography className="answer">
-                                {ans}
+                                {answer}
                             </Typography>
                         </AccordionDetails>
                     </Accordion>
