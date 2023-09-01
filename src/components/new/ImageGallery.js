3176bb889a543cfc423ed705a9a0a95bc10692c3
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2023-09-01 18:13:54 +0530

    feat : image gallery component

---
+++ src/components/new/ImageGallery.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+const ImageGallery = () => {
+    const [currImage , setCurrImage] = useState(0)
+    const images = ['/new-assets/placeholder.png','/new-assets/books-donated-icon.svg','/new-assets/placeholder.png','/new-assets/placeholder.png','/new-assets/placeholder.png']
+    console.log(currImage)
+  return (
+    <div className='image-gallery'>
+        <img className='main-image' src= {images[currImage]} />
+        <div className='image-options'>
+        {
+            images.map((src , index) => (
+                <img key={src + "" + index} onClick={() => setCurrImage(index)} src={src} />
+            ))
+        }
+        </div>
+    </div>
+  )
+}
+
+export default ImageGallery
