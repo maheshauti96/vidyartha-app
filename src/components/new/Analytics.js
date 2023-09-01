fb6d05be31a2b59cff3789db943f606e596231a1
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2023-09-01 18:12:30 +0530

    feat : analytics component

---
+++ src/components/new/Analytics.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Analytics = () => {
+  return (
+    <div className='analytics'>
+        <div>
+            <img src='./new-assets/fundraiser-icon-1.svg' />
+            <p className='analytics-title'>Current Fundraisers</p>
+            <p className='analytics-num' >100+</p>
+        </div>
+        <div>
+            <img src='./new-assets/fundraiser-icon-2.svg' />
+            <p className='analytics-title'>Successful fundraisers</p>
+            <p className='analytics-num' >100+</p>
+        </div>
+        <div>
+            <img src='./new-assets/books-donated-icon.svg' />
+            <p className='analytics-title'>Books donated</p>
+            <p className='analytics-num' >100+</p>
+        </div>
+        <div>
+            <img src='./new-assets/money-raised-icon.svg' />
+            <p className='analytics-title'>Money raised</p>
+            <p className='analytics-num' >100+</p>
+        </div>
+        <div>
+            <img src='./new-assets/books-donated-icon.svg' />
+            <p className='analytics-title'>Schools helped</p>
+            <p className='analytics-num' >100+</p>
+        </div>
+    </div>
+  )
+}
+
+export default Analytics
