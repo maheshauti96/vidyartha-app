1eb8ab75810ecb561cc5c0454fb50c3c5e6c9e1d
Mahesh Auti <maheshauti00@gmail.com>
2023-11-30 10:59:54 +0530

    [fixed top donors list]

--- src/components/new/TopDonors.js
+++ src/components/new/TopDonors.js
@@ -1,51 +1,45 @@
 import { EmojiEventsOutlined } from "@material-ui/icons";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 const TopDonors = ({ topDonors }) => {
-  const [donors, setDonors] = useState();
-  useEffect(() => {
-    setDonors(topDonors)
-  } , [])
+  const [showAll, setShowAll] = useState(false); // state to toggle between show more and show less
+
+  const displayedDonors = showAll ? topDonors : topDonors.slice(0, 2); // Show first 2 or all donors based on state
 
   return (
     <>
-      {donors && donors.length > 0 && (
+      {topDonors && topDonors.length > 0 && (
         <div className="top-donors">
           <h3 className="title">
             <EmojiEventsOutlined />
             Top Donors
           </h3>
           <div className="donors-list">
-            {donors.map((val, index) =>
-              index < donors.num ? (
-                <div className="donors">
+            {displayedDonors.map((donor, index) =>
+              donor ? (
+                <div key={index} className="donors"> {/* Added key for list rendering */}
                   <img
                     className="donor-img"
                     src="/new-assets/testimonial-user.jpeg"
+                    alt="Donor"
                   />
                   <div>
-                    <p className="amount">Rs. 1200</p>
-                    <p className="user"> By Lorem Ipsum</p>
+                    <p className="amount"> {donor.amount} </p>
+                    <p className="user"> {donor.name}</p>
                   </div>
                 </div>
               ) : null
             )}
           </div>
 
-          <button
-            className="view-all"
-            onClick={() => {
-              setDonors((prev) => {
-                if (prev.num === 2) {
-                  return { num: donorsList.length, clicked: true };
-                } else {
-                  return { num: 2, clicked: false };
-                }
-              });
-            }}
-          >
-            View {donors.clicked ? "less" : "more"}
-          </button>
+          {topDonors.length > 2 && (  // Only show button if there are more than 2 donors
+            <button
+              className="view-all"
+              onClick={() => setShowAll(prev => !prev)}  // Toggle the showAll state
+            >
+              View {showAll ? "less" : "more"}
+            </button>
+          )}
         </div>
       )}
     </>
